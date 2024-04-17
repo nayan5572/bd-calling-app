ed6f2f256ab700aeab9413430ba97925aa09304a
nayan <nayan.dev572@gmail.com>
2024-04-17 07:55:50 +0600

    home page done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../Pages/Shared/Navbar/Navbar";
+
+
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
